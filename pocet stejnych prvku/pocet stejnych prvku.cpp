07d80f8723c8289d90669095d826a62057da9a25
Vojtech Hruby <vojtech.hruby.020@pslib.cz>
2022-04-11 12:05:18 +0200

    Přidejte soubory projektu.

---
+++ pocet stejnych prvku/pocet stejnych prvku.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main() {
+	int pocet[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int cisla[10];
+
+	int znak = 0;
+	int cislo = 0;
+
+	FILE* soubor = fopen("cisla.txt", "r");
+
+	if (soubor == NULL) {
+		printf("Chyba: Soubor nelze otevrit pro cteni.\n");
+	}
+
+	while (!feof(soubor)) {
+		fscanf(soubor, "%d", &cisla[znak]);
+		znak++;
+	}
+	for (int i = 0; i < 10; i++) {
+		cislo = cisla[i];
+		pocet[cislo]++;
+	}
+	for (int i = 0; i < 10; i++) printf("Cislo %d se vyskytuje v souboru %dx\n", i, pocet[i]);
+
+
+	if (fclose(soubor) == EOF) {
+		printf("Chyba: Soubor nelze zavrit.\n");
+	}
+}
